43b6076b9b5a2e86b1dba2d38732253274547494
mahathi500 <mahathinalagandla500@gmail.com>
2026-08-13 14:31:00 +0530

    first commit

---
+++ Factorial.java
@@ -0,0 +1,11 @@
+public class Factorial{
+	public static long factorial(int n){
+		if (n<0) throw new IllegalArgumentException("negative number not allowed");
+		long result=1;
+		for(int i=2;i<=n;i++){
+			result*=i;
+		}
+		return result;
+	}
+
+}
